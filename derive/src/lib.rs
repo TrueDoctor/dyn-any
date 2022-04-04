008be6b990a43319c68246851caef8babd87db3b
Dennis <dennis@kobert.dev>
2022-04-04 23:34:41 +0200

    Implement DynAny

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -6,7 +6,10 @@ use proc_macro::TokenStream;
 use proc_macro2::Span;
 use proc_macro_roids::{DeriveInputStructExt, FieldExt, IdentExt};
 use quote::quote;
-use syn::{parse_macro_input, parse_quote, DeriveInput, Ident, Lifetime, Type};
+use syn::{
+    parse_macro_input, parse_quote, DeriveInput, GenericParam, Ident, Lifetime,
+    LifetimeDef, Type, TypeParamBound,
+};
 
 /// Derives an implementation for the [`DynAny`] trait.
 ///
@@ -59,43 +62,47 @@ pub fn system_desc_derive(input: TokenStream) -> TokenStream {
     let generics = &ast.generics;
     let attrs = &ast.attrs;
 
-    let variants = fields
-        .iter()
-        .map(|field| {
-            let ident = field.ident.clone();
-            let vis = field.vis.clone();
-            let ty = field.ty.clone();
-            let ty = match ty {
-                Type::Reference(r) => {
-                    let mut r = r;
-                    r.lifetime =
-                        Some(Lifetime::new("'static", Span::call_site()));
-                    Type::Reference(r)
-                }
-                t => t,
-            };
-            if let Some(ident) = ident {
-                quote! {#vis #ident: #ty}
-            } else {
-                quote! {#vis #ty}
-            }
-        })
-        .collect::<Vec<_>>();
-    let tuple_struct = fields.iter().any(|field| field.ident.is_none());
+    let static_params = replace_lifetimes(generics, "'static");
+    let dyn_params = replace_lifetimes(generics, "'dyn_any");
 
-    let fields = if tuple_struct {
-        quote! {
-            ( #(#variants,)* )
-        }
-    } else {
-        quote! {
-             {   #(#variants,)* }
-        }
-    };
+    let old_params = &generics.params.iter().collect::<Vec<_>>();
     let foo = quote! {
-        #(#attrs )*
-        #vis struct #struct_name #generics #fields
+        impl<'dyn_any, #(#old_params,)*> DynAny<'dyn_any> for #struct_name <#(#dyn_params,)*> {
+            type Static =  #struct_name <#(#static_params,)*>;
+        }
     };
     //panic!("{:?}", foo.to_string());
     TokenStream::from(foo)
+}
+
+fn replace_lifetimes(
+    generics: &syn::Generics,
+    replacement: &str,
+) -> Vec<proc_macro2::TokenStream> {
+    let params = generics
+        .params
+        .iter()
+        .map(|param| {
+            let param = match param {
+                GenericParam::Lifetime(_) => {
+                    GenericParam::Lifetime(LifetimeDef::new(Lifetime::new(
+                        replacement,
+                        Span::call_site(),
+                    )))
+                }
+                GenericParam::Type(t) => {
+                    let mut t = t.clone();
+                    t.bounds.iter_mut().for_each(|bond| {
+                        if let TypeParamBound::Lifetime(ref mut t) = bond {
+                            *t = Lifetime::new(replacement, Span::call_site())
+                        }
+                    });
+                    GenericParam::Type(t.clone())
+                }
+                c => c.clone(),
+            };
+            quote! {#param}
+        })
+        .collect::<Vec<_>>();
+    params
 }
